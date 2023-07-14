6e5e4890a6f01e1f434f16c4101e493462a3bf95
cristian <cristian_fabian_pesse@hotmail.com>
2023-07-14 15:55:43 -0300

    Realice layout_delete, Agregue background_delete en drawable y agregue texto en strings

---
+++ app/src/main/java/com/example/freeshop/ui/DeleteActivity.kt
@@ -0,0 +1,12 @@
+package com.example.freeshop.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.freeshop.R
+
+class DeleteActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_delete)
+    }
+}
