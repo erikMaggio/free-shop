b9120c053b6299c56125bbdcfa8abd5cb6e37056
erik maggio <erik97@hotmail.com.ar>
2023-07-18 16:32:21 -0300

    creacion recycler view con onclick

---
+++ app/src/main/java/com/example/freeshop/model/response/Title.kt
@@ -0,0 +1,3 @@
+package com.example.freeshop.model.response
+
+data class Title(val title: String, var image: Int)
--- app/src/main/java/com/example/freeshop/ui/AddActivity.kt
+++ app/src/main/java/com/example/freeshop/ui/AddActivity.kt
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.freeshop.R
 
-class addActivity : AppCompatActivity() {
+class AddActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add)
